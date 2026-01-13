a1761ac1d3fd12cfb5ebd748a92ecfb86614f950
“Felipe-PS-95” <ffelipes10@gmail.com>
2026-01-13 01:40:29 -0300

    24. Criando componente de menu com links e ícones

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
   console.log("Oi");
 
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
-        repellat, odio dicta deserunt aliquid sequi consectetur unde modi iusto
-        inventore maxime sapiente aspernatur nulla voluptas sed. Error
-        reprehenderit libero beatae?
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
     </>
   );
 }
